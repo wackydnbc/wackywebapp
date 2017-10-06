f51bb2d9539269e3cda6251fddc67294a2464114
Daniel Capacio <danielcapacio@gmail.com>
2017-10-06 01:08:46 -0700

    Cleaned up homepage scaffolding

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -9,6 +9,7 @@
     {/menudata}
 </ul>
 <ul class="nav navbar-nav navbar-right">
+    <!-- TODO: maybe the optional about page can go here, if we have time -->
     <li><a href="#">Placeholder</a></li>
     <li><a href="#">Placeholder</a></li>
 </ul>
