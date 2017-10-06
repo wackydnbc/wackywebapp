e5a35a37af4047d759e8fa5ac655969217a854fc
Daniel Capacio <danielcapacio@gmail.com>
2017-10-06 01:03:17 -0700

    Set favicon to 32x32

--- application/views/template.php
+++ application/views/template.php
@@ -20,7 +20,7 @@ if (!defined('APPPATH'))
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
     <link rel="stylesheet" type="text/css" href="/assets/css/style.css"/>
-    <link rel="icon" href="assets/images/xwing.png"/>
+    <link rel="icon" href="assets/images/xwing-favicon-32x32.png"/>
 </head>
 <body>
 <div class="container">
