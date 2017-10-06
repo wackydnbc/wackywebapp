e23237962059f6a254a1c62bb8cae1f96774f399
Bryn Beaudry <babeaudry@gmail.com>
2017-10-05 17:01:10 -0700

    added flight and fleet to autoload, made flights controller

--- application/controllers/FlightsController.php
+++ application/controllers/FlightsController.php
@@ -20,7 +20,7 @@ class Welcome extends Application {
 	 */
 	public function index()
 	{
-		$this->data['pagebody'] = 'homepage';
+		$this->data['pagebody'] = $this->flights;
 
 		$this->render();
 	}
