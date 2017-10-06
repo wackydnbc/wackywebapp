55a0747dd3cfd25a4851898a3c5b62c10e1e030e
Daniel Capacio <danielcapacio@gmail.com>
2017-10-06 01:22:22 -0700

    Initial fleet page setup

---
+++ application/views/fleet.php
@@ -0,0 +1,110 @@
+<div class="panel panel-default">
+	<div class="panel-heading">Airplanes</div>
+	<div class="panel-body">
+		<table class="table table-striped table-hover">
+			<thead>
+			<tr>
+				<th>#</th>
+				<th>ID</th>
+				<th>Manufacturer</th>
+				<th>Model</th>
+                <th>Price</th>
+                <th>Seats</th>
+                <th>Reach</th>
+                <th>Cruise</th>
+                <th>Takeoff</th>
+                <th>Hourly</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td>1</td>
+                <!--
+                TODO: clicking on a plane's identifier should trigger a separate page with everything you can determine or show for that plane
+                -->
+				<td><a href="#linkToSeparatePageShowingPlaneInfo">Data</a></td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+			</tr>
+			<tr>
+				<td>2</td>
+                <td><a href="#linkToSeparatePageShowingPlaneInfo">Data</a></td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+			</tr>
+			<tr class="info">
+				<td>3</td>
+                <td><a href="#linkToSeparatePageShowingPlaneInfo">Data</a></td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+			</tr>
+			<tr class="success">
+				<td>4</td>
+                <td><a href="#linkToSeparatePageShowingPlaneInfo">Data</a></td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+			</tr>
+			<tr class="danger">
+				<td>5</td>
+                <td><a href="#linkToSeparatePageShowingPlaneInfo">Data</a></td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+			</tr>
+			<tr class="warning">
+				<td>6</td>
+                <td><a href="#linkToSeparatePageShowingPlaneInfo">Data</a></td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+			</tr>
+			<tr class="active">
+				<td>7</td>
+                <td><a href="#linkToSeparatePageShowingPlaneInfo">Data</a></td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+			</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
