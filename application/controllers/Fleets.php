f81e47db8519b0ebf7052437d469e1b281942baf
Daniel Capacio <danielcapacio@gmail.com>
2017-10-06 13:06:54 -0700

    Closes #18 - renaming fleets controller and routes to resolve conflicts

--- application/controllers/Fleets.php
+++ application/controllers/Fleets.php
@@ -1,12 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Fleet extends Application {
+class Fleets extends Application {
 
 	public function index()
 	{
 		$this->data['pagebody'] = 'fleet';
-		$this->data['pagetitle'] = 'XWing Fleet Page';
+		$this->data['pagetitle'] = 'XWing Fleets Page';
 		$this->render();
 	}
 }
