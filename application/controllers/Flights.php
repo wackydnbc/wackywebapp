19f5e09b74df0a6cd3df61d615f1390971fdfae2
Bryn Beaudry <babeaudry@gmail.com>
2017-10-05 17:38:18 -0700

    added flights view and routing for flights

--- application/controllers/Flights.php
+++ application/controllers/Flights.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends Application {
+class Flights extends Application {
 
 	/**
 	 * Index Page for this controller.
@@ -20,8 +20,8 @@ class Welcome extends Application {
 	 */
 	public function index()
 	{
-		$this->data['pagebody'] = $this->flights;
-
+		$this->data['flights'] = $this->flight->all();
+		$this->data['pagebody'] = 'Flights';
 		$this->render();
 	}
 }
