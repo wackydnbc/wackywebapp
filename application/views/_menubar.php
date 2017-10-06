8ca358bb33f5d58d76917f22aed4b7763d87731b
Daniel Capacio <danielcapacio@gmail.com>
2017-10-06 01:02:09 -0700

    Navbar changes, logo sizing, full page background coverage

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -3,8 +3,12 @@
  * Menu navbar, just an unordered list
  */
 ?>
-<ul class="nav">
+<ul class="nav navbar-nav">
     {menudata}
     <li><a href="{link}">{name}</a></li>
     {/menudata}
+</ul>
+<ul class="nav navbar-nav navbar-right">
+    <li><a href="#">Placeholder</a></li>
+    <li><a href="#">Placeholder</a></li>
 </ul>
