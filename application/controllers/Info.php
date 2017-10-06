39c8e6fc6030f513e3a6d09c74a6cddc5160565a
Daniel Capacio <danielcapacio@gmail.com>
2017-10-06 16:19:00 -0700

    Info controller done with fleet and flights subcontrollers

---
+++ application/controllers/Info.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Info extends Application
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+	    $this->data['pagebody'] = 'info';
+		$this->data['pagetitle'] = 'Info';
+	    $this->render();
+	}
+
+	public function fleet()
+	{
+		header('Content-Type: application/json');
+		echo json_encode($this->plane->all(), JSON_PRETTY_PRINT);
+	}
+
+	public function flights()
+	{
+		header('Content-Type: application/json');
+		echo json_encode($this->flight->all(), JSON_PRETTY_PRINT);
+	}
+}
