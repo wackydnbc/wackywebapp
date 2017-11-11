4962fb3d9c6c090885230214dc4ca377277b21f8
Daniel Capacio <danielcapacio@gmail.com>
2017-11-10 22:11:29 -0800

    Fixed model naming in autoload for flights and planes list

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,5 +132,5 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('flight', 'plane', 'flightslist', 'planeslist');
+$autoload['model'] = array('flight', 'plane', 'flightsList', 'planesList');
 
