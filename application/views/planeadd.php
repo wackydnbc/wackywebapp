99197fbff616d181b3cc812c2bd528bb043c1d0f
Niko Arellano <narellano@my.bcit.ca>
2017-11-13 16:14:58 -0800

    Finalized add/edit/delete plane feature

--- application/views/planeadd.php
+++ application/views/planeadd.php
@@ -1,1 +1,8 @@
-<a href="/fleet/add"><input type="button" value="Add a new plane"/></a>
+<h1>Add Plane</h1>
+<form role="form" action="/fleet/submit" method="post">
+
+    {fplanecode}<br />
+    {zsubmit}
+</form>
+{error}
+<a href="/fleet/cancel"><input type="button" value="Cancel the current edit"/></a>
